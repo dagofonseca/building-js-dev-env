8c3bb5652be63cf6736a81f534178e00b187787f
dagofonseca <dagofonseca6@gmail.com>
2019-05-16 17:13:34 -0500

    build works

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,25 +1,30 @@
 var path = require('path');
+var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 export default {
   debug: true,
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   noInfo: false,
   entry: [
     path.resolve(__dirname, 'src/index')
   ],
   target: 'web',
   output: {
-    path: path.resolve(__dirname, 'src'),
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
     filename: 'bundle.js'
   },
   plugins: [
-     //Create HTML file that includes reference to bundle JS.
-     new HtmlWebpackPlugin({
+    //Create HTML file that includes reference to bundle JS.
+    new HtmlWebpackPlugin({
       template: 'src/index.html',
       inject: true
-    })
+    }),
+    //Eliminate duplicate packages when generating bundle
+    new webpack.optimize.DedupePlugin(),
+    //Minify JS
+    new webpack.optimize.UglifyJsPlugin()
   ],
   module: {
     loaders: [
